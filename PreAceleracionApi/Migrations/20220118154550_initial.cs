72374bd7094559dce08b9abe4b2dd21838acc991
Lucas Emanuel Medina <medinalucas16@gmail.com>
2022-01-18 13:37:50 -0300

    Creada la bd y migradad

---
+++ PreAceleracionApi/Migrations/20220118154550_initial.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PreAceleracionApi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    usuarioid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Comment_User_usuarioid",
+                        column: x => x.usuarioid,
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    usuarioid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Post_User_usuarioid",
+                        column: x => x.usuarioid,
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_usuarioid",
+                table: "Comment",
+                column: "usuarioid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_usuarioid",
+                table: "Post",
+                column: "usuarioid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comment");
+
+            migrationBuilder.DropTable(
+                name: "Post");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ PreAceleracionApi/Migrations/20220118163339_users.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PreAceleracionApi.Migrations
+{
+    public partial class users : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_User_usuarioid",
+                table: "Comment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_User_usuarioid",
+                table: "Post");
+
+            migrationBuilder.RenameColumn(
+                name: "usuarioid",
+                table: "Post",
+                newName: "userid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Post_usuarioid",
+                table: "Post",
+                newName: "IX_Post_userid");
+
+            migrationBuilder.RenameColumn(
+                name: "usuarioid",
+                table: "Comment",
+                newName: "userid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_usuarioid",
+                table: "Comment",
+                newName: "IX_Comment_userid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_User_userid",
+                table: "Comment",
+                column: "userid",
+                principalTable: "User",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_User_userid",
+                table: "Post",
+                column: "userid",
+                principalTable: "User",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_User_userid",
+                table: "Comment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_User_userid",
+                table: "Post");
+
+            migrationBuilder.RenameColumn(
+                name: "userid",
+                table: "Post",
+                newName: "usuarioid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Post_userid",
+                table: "Post",
+                newName: "IX_Post_usuarioid");
+
+            migrationBuilder.RenameColumn(
+                name: "userid",
+                table: "Comment",
+                newName: "usuarioid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_userid",
+                table: "Comment",
+                newName: "IX_Comment_usuarioid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_User_usuarioid",
+                table: "Comment",
+                column: "usuarioid",
+                principalTable: "User",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_User_usuarioid",
+                table: "Post",
+                column: "usuarioid",
+                principalTable: "User",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
